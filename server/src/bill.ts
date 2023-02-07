69ba7a71ae815d4ac37c001d94c3c539a7b7b467
Your Name <jacob4seinfeld@gmail.com>
2023-02-06 16:19:38 -0800

    switch with semicolons

--- server/src/bill.ts
+++ server/src/bill.ts
@@ -2,22 +2,22 @@ import * as mongodb from "mongodb";
 
 // Payees included in bill: their name, the list of items they got, and the total they owe
 interface Payee {
-  _name: String,
-  _orders: Array<Number>, // Array of indices from _orders property in Bill
-  _orderTotal: Number
+  _name: String;
+  _orders: Array<Number>; // Array of indices from _orders property in Bill
+  _orderTotal: Number;
 }
 
 interface Item {
-  _desc: String,
-  _qty: Number,
-  _price: Number
+  _desc: String;
+  _qty: Number;
+  _price: Number;
 }
 
 export interface Bill {
-   _orders: Array<Item>,
-   _tip: Number,
-   _tax: Number,
-   _total: Number,
-   _payees?: Array<Payee>,
+   _orders: Array<Item>;
+   _tip: Number;
+   _tax: Number;
+   _total: Number;
+   _payees?: Array<Payee>;
    _id?: mongodb.ObjectId;
 }
