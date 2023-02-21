5607c4a59e6464ccfd1170cc867d74ede776827a
Clemente Farias Canepa <cbfariasc@gmail.com>
2023-02-21 04:11:51 -0800

    Working skeleton to allow frontend to switch to different pages

---
+++ frontend-react-draft/src/components/Page3.js
@@ -0,0 +1,35 @@
+import './App.css';
+import { Link } from 'react-router-dom';
+import React from 'react';
+
+class Page3 extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+          <div className="App">
+              <header className="App-header">
+                <p>
+                  Hello! Welcome to Page 3.
+
+                  : )
+                </p>
+
+                <form id="form-img-upload" accept="image/png, image/jpeg, image/jpg">
+                <input type="image" id="input-img-upload" multiple={false} />
+                <label id="label-img-upload" htmlFor="input-img-upload">
+                  <div>
+                    <p id="form-img-text">Drop your image here or</p>
+                    <Link to="/page4/"><button className="upload-button">Upload</button></Link>
+                  </div> 
+                </label>
+              </form>
+              </header>
+          </div>
+    );
+  }
+}
+
+export default Page3;
