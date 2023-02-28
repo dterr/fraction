f693b4a511493d16df7a427e6f3ba25f6a393b6f
dterr <dterrones31@gmail.com>
2023-02-28 00:57:13 -0800

    Revert processReceipt due to incomp w TS

--- frontend-react-draft/src/processReceipt.js
+++ frontend-react-draft/src/processReceipt.js
@@ -1,10 +1,50 @@
-const helpers = require('./helpers');
-const Bill = require('./bill').Bill;
+const axios = require('axios');
+//const { receiveMessageOnPort } = require('worker_threads');
 
-const processReceipt = async (filePath) => {
-  const receiptBody = await helpers.getOCR(5000, filePath);
-  const bill = helpers.convertOCRToBill(receiptBody, 0);
-  return new Bill(bill);
+const apiKey = process.env.TABSCANNER_API_KEY;
+const endpoint = 'https://api.tabscanner.com/api/v2/process';
+
+var Receipt = require("./schema/receipt")
+
+const processReceipt = async (req) => {
+//   const picture = req.uploadedphoto;
+//   const formData = new FormData();
+//   formData.append('file', fs.createReadStream(filePath));
+
+  const config = {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+      'X-API-KEY': apiKey,
+    },
+  };
+  console.log(req.uploadedphoto);
+  const response = await axios.post(endpoint, req, config);
+  const filteredData = filterReceiptData(response.data);
+  const newReceipt = new Receipt(filteredData);
+  await newReceipt.save();
+  console.log("Receipt saved to database:", newReceipt);
+  return newReceipt;
 };
+
+const filterReceiptData = (data) => {
+    const filteredData = {
+      establishment: data.establishment,
+      total: data.total,
+      subtotal: data.subtotal,
+      cash: data.cash,
+      change: data.change,
+      tax: data.tax,
+      tip: data.tip,
+      currency: data.currency,
+      lineItems: []
+    };
+  
+    for (const lineItem of data.lineItems) {
+      const { lineTotal, desc, qty, price, unit } = lineItem;
+      filteredData.lineItems.push({ lineTotal, desc, qty, price, unit });
+    }
+  
+    return filteredData;
+  };
 
 module.exports = processReceipt;
