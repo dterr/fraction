899c65629795d52b3b36f7e8d96b2d2deb44318e
dterr <dterrones31@gmail.com>
2023-02-28 17:20:53 -0800

    Slight functionality but err on boundaries.

--- frontend-react-draft/src/webServer.js
+++ frontend-react-draft/src/webServer.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const multer  = require('multer');
 const upload = multer({ dest: 'uploads/' });
+const axios = require('axios');
+
 // const helpers = require('frontend-react-draft/server/src/helpers.ts');
 // const { convertOCRToBill } = require('../server/src/helpers.ts');
 // const processReceipt = require('./processReceipt.js');
@@ -17,15 +19,19 @@ const processReceipt = async (req) => {
     //   const picture = req.uploadedphoto;
     //   const formData = new FormData();
     //   formData.append('file', fs.createReadStream(filePath));
-    
+    const apiKey = "hardCoded rn";
+    console.log("Inside processReceipt: \n");    
       const config = {
         headers: {
-          'Content-Type': 'multipart/form-data',
-          'X-API-KEY': apiKey,
+          'Content-Type': `multipart/form-data`,
+          'apikey': apiKey,
+          'timeout': 30000,
         },
       };
-      console.log(req.uploadedphoto);
+      const endpoint = 'https://api.tabscanner.com/api/v2/process';
+      console.log("Image:", req.file);
       const response = await axios.post(endpoint, req, config);
+      console.log("Axios Response from API:", response);
       const filteredData = filterReceiptData(response.data);
       const newReceipt = new Receipt(filteredData);
       await newReceipt.save();
@@ -56,9 +62,10 @@ const processReceipt = async (req) => {
 
 /* EXPRESS METHODS */
 
-app.post('/api/receipt', upload.single('receipt'), async (req, res) => {
+app.post('/api/receipt', upload.single('file'), function (req, res) {
   try {
-    console.log("Going to try getOCR", req);
+    console.log("Going to try getOCR", req.file);
+    const filteredData = processReceipt(req);
     
     // const receiptBody = await helpers.getOCR(10000, req.uploadedphoto);
     // const bill = convertOCRToBill(receiptBody, 0);
